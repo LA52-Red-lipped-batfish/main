e04472a83420cf2e891cdc762355a206b00818a9
Patrick <patrickhu91@gmail.com>
2022-08-12 19:17:42 -0700

    setup webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,59 @@
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const htmlPlugin = new HtmlWebpackPlugin({
-  
+  filename: 'webpack.html',
+  template: './client/index.html'
 })
+
 module.exports = {
-  entry: 'index.js',
+  mode: process.env.NODE_ENV,
+  entry: { 
+    src: './client/index.js' 
+  },
   output: {
-    path: __dirname + '/dist',
-    filename: 'index_bundle.js'
+    path: path.join(__dirname, 'build'),
+    publicPath: '/',
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              ['@babel/preset-env', '@babel/preset-react']
+            ]
+          }
+        }
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        exclude: /node_modules/,
+        use: [
+          "style-loader",
+          "css-loader",
+          {
+            loader: "sass-loader",
+            options: {
+              implementation: require.resolve("sass"),
+            }
+          }
+        ]
+      }
+    ]
   },
   plugins: [
-    new HtmlWebpackPlugin()
-  ]
+    htmlPlugin
+  ],
+  resolve: {
+    extensions: ['.js', '.jsx']
+  },
+  devServer: {
+    proxy: {
+      '/': 'localhost:3000'
+    }
+  }
 }
